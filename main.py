368de212c63c17c478765988301e796cc8dc8288
Sinus2x <hatin.max2002@gmail.com>
2022-07-30 22:31:27 +0300

    add run with environment variable

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import logging
+import os
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils import executor
 
-from config import BOT_TOKEN
-
 logging.basicConfig(level=logging.INFO)
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = Bot(token=BOT_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-
 
 if __name__ == '__main__':
     from handlers import dp
